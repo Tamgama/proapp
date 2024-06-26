36b9f4c0c3f498b58b8fc1e1fb2ca5ff60a07423
Tamgama <tamara.garmart@gmail.com>
2024-06-26 14:49:55 +0200

    df

--- lib/main copy.dart
+++ lib/main copy.dart
@@ -2,7 +2,6 @@ import 'dart:ui';
 
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:proapp/main.dart';
 import 'package:provider/provider.dart';
 
 void main() {
